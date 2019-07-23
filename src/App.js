26cde2a89f1ec39c3eb61a7a8acb77e00355892a
bernadette breboneria <bernadette.breboneria@biarri.com>
2019-07-23 14:59:18 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './component/header/index';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
